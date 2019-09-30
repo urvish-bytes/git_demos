b2f321dd1ba59df8bb5e031e27f46e17584f2cda
urvish <urvish.patel@bytestechnolab.com>
2019-09-30 14:51:45 +0530

    log off

---
+++ multix/application/logs/log-2019-09-30.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-30 14:33:18 --> 404 Page Not Found: Public/admin
+ERROR - 2019-09-30 14:33:18 --> 404 Page Not Found: Public/admin
