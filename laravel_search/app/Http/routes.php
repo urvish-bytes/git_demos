b4f9ff4a7fda28276e64cc30bb230db1969f6d6e
urvish <urvish.patel@bytestechnolab.com>
2019-12-17 15:27:21 +0530

    modal demo

--- laravel_search/app/Http/routes.php
+++ laravel_search/app/Http/routes.php
@@ -1,19 +1,29 @@
 <?php
 use App\User;
 use Illuminate\Support\Facades\Input;
+
 Route::get ( '/', function () {
-	$dummyDetails = User::paginate(15);
-	return view ( 'welcome' )->withUsers($dummyDetails);
+	// $dummyDetails = User::paginate(15);
+	return view ( 'welcome' );
 } );
-Route::any ( '/search', function () {
-	$q = Input::get ( 'q' );
-	if($q != ""){
-	$user = User::where ( 'name', 'LIKE', '%' . $q . '%' )->orWhere ( 'email', 'LIKE', '%' . $q . '%' )->paginate (5)->setPath ( '' );
-	$pagination = $user->appends ( array (
-				'q' => Input::get ( 'q' ) 
-		) );
-	if (count ( $user ) > 0)
-		return view ( 'welcome' )->withDetails ( $user )->withQuery ( $q );
-	}
-		return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
-} );
+
+// Route::any ( '/search', function () {
+		// $q = Input::get( 'q' );
+		// // if($q != ""){
+		// $user = dummyDetails::where ( 'name', 'LIKE', '%' . $q . '%' )->orWhere ( 'email', 'LIKE', '%' . $q . '%' )->paginate (5)->setPath ( '' );
+		// $pagination = $user->appends ( array (
+		// 			'q' => Input::get ( 'q' ) 
+		// 	) );
+		// 	if (count ( $user ) > 0)
+		// 	return view ( 'welcome' )->withDetails ( $user )->withQuery ( $q );
+		// // }
+		// 	return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
+// } );
+
+Route::any('/search',function(){
+    $q = Input::get ('q');
+    $user = User::where('name','LIKE','%'.$q.'%')->orWhere('email','LIKE','%'.$q.'%')->get();
+    if(count($user) > 0)
+        return view('welcome')->withDetails($user)->withQuery ( $q );
+    else return view ('welcome')->withMessage('No Details found. Try to search again !');
+});
--- laravel_search/resources/views/welcome.blade.php
+++ laravel_search/resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Search functionality - justLaravel.com</title>
+<title>Search Demo</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet"
 	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
+
 	<div class="container">
 		<form action="/search" method="POST" role="search">
 			{{ csrf_field() }}
@@ -21,9 +22,37 @@
 			</div>
 		</form>
 	</div>	
+
 	<div class="container">
-	@if(isset($users))
-	<h2>Sample User details</h2>
+		@if(isset($details))
+		<p> The Search results for your query <b> {{ $query }} </b> are :</p>
+		<h2>Sample User details</h2>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($details as $user)
+				<tr>
+					<td>{{$user->name}}</td>
+					<td>{{$user->email}}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		
+		@if($details){!! $details->render() !!}@endif
+		@elseif(isset($message))
+		<p>{{ $message }}</p>
+		@endif
+	</div>
+		
+	<div class="container">
+		@if(isset($users))
+		<h2>Sample User details</h2>
 		<table class="table table-striped">
 			<thead>
 				<tr>
@@ -43,31 +72,6 @@
 		</table>
 		{!! $users->render() !!}@endif
 	</div>
-		<div class="container">
-			@if(isset($details))
-			<p> The Search results for your query <b> {{ $query }} </b> are :</p>
-			<h2>Sample User details</h2>
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($details as $user)
-					<tr>
-						<td>{{$user->name}}</td>
-						<td>{{$user->email}}</td>
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-			
-			@if($details){!! $details->render() !!}@endif
-			@elseif(isset($message))
-			<p>{{ $message }}</p>
-			@endif
-		</div>
+
 </body>
 </html>
