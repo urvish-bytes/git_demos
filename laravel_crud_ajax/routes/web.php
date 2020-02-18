43ae8369fe89ae616a89456f9e0cc9861779d767
urvish <urvish.patel@bytestechnolab.com>
2020-02-18 18:06:51 +0530

    upload setup

--- laravel_crud_ajax/routes/web.php
+++ laravel_crud_ajax/routes/web.php
@@ -15,11 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::resource('crud_ajax','AjaxController');
-Route::match(['get', 'post'], 'crud_ajax', (['as' => 'crud_ajax', 'uses' => 'AjaxController@index']));
-// Route::match(['get', 'post'], 'user_create', (['as' => 'user_create', 'uses' => 'AjaxController@create']));
-Route::match(['get', 'post'], 'user_edit/{id}', (['as' => 'user_edit', 'uses' => 'AjaxController@edit']));
-Route::match(['get', 'post'], 'user_store', (['as' => 'user_store', 'uses' => 'AjaxController@store']));
-// Route::match(['get', 'post'], 'user_update/{id}', (['as' => 'user_update', 'uses' => 'AjaxController@update']));
-// Route::match(['get', 'post'], 'user_destroy/{id}', (['as' => 'user_destroy', 'uses' => 'AjaxController@destroy']));
-Route::delete('user_destroy/{id}', (['as' => 'user_destroy', 'uses' => 'AjaxController@destroy']));
+Route::resource('crud_ajax','AjaxController');
+// Route::match(['get', 'post'], 'crud_ajax', (['as' => 'crud_ajax', 'uses' => 'AjaxController@index']));
+// // Route::match(['get', 'post'], 'user_create', (['as' => 'user_create', 'uses' => 'AjaxController@create']));
+// Route::match(['get', 'post'], 'user_edit/{id}', (['as' => 'user_edit', 'uses' => 'AjaxController@edit']));
+// Route::match(['get', 'post'], 'user_store', (['as' => 'user_store', 'uses' => 'AjaxController@store']));
+// // Route::match(['get', 'post'], 'user_update/{id}', (['as' => 'user_update', 'uses' => 'AjaxController@update']));
+// // Route::match(['get', 'post'], 'user_destroy/{id}', (['as' => 'user_destroy', 'uses' => 'AjaxController@destroy']));
+// Route::delete('user_destroy/{id}', (['as' => 'user_destroy', 'uses' => 'AjaxController@destroy']));
